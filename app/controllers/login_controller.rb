1d0b51323726b607ee0ee5d83437df34e6a50ef9
tknzk <tkm.knzk@gmail.com>
2017-11-09 14:44:08 +0900

    auth

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,7 +4,8 @@ class LoginController < ApplicationController
   end
 
   def auth
-    val = params.permit(:name, :password)
+    # binding.pry
+    val = params.permit(:email, :name, :password)
 
     client = Aws::CognitoIdentityProvider::Client.new(
       region: ENV['AWS_REGION'],
@@ -12,22 +13,27 @@ class LoginController < ApplicationController
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     )
 
-    resp = client.sign_up({
-      client_id: ENV['AWS_COGNITO_APP_CLIENT_ID'],
-      # secret_hash: ENV['AWS_COGNITO_APP_CLIENT_SECRET'],
-      username: val['name'],
-      password: val['password'],
-      user_attributes: [
-        {
-          name: 'email',
-          value: val['email'],
-        },
-        {
-          name: 'name',
-          value: val['name'],
-        }
-      ]
+    # signin
+   #  @resp = client.initiate_auth({
+   #    client_id: ENV['AWS_COGNITO_APP_CLIENT_ID'],
+   #    auth_flow: 'USER_SRP_AUTH',
+   #    auth_parameters: {
+   #      USERNAME: val['name'],
+   #      PASSWORD: val['password'],
+   #    }
+   #  })
+    # cog_provider.initiate_auth({client_id: "xxxxxxxxx", auth_parameters: { "name" => "xxx", "password" => "xxx"}, auth_flow: "USER_SRP_AUTH"})
+
+    # SRP_A ではなく admin_auth_initiate で plain な passwordを送信
+    @resp = client.admin_initiate_auth({
+      auth_flow: 'ADMIN_NO_SRP_AUTH',
+      auth_parameters: {
+        USERNAME: val['name'],
+        PASSWORD: val['password'],
+      },
+      user_pool_id: ENV['AWS_COGNITO_POOL_ID'],
+      client_id: ENV['AWS_COGNITO_APP_CLIENT_ID']
     })
-    redirect_to '/signup/confirm'
+
   end
 end
